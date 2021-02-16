463b9d51f40c01bbf573c26892d339bbef5759f7
Germán Lugo <g.lugo.dev@gmail.com>
2021-02-16 10:14:55 +0100

    Add component.

--- frontend/src/components/WelcomeBanner.js
+++ frontend/src/components/WelcomeBanner.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function WelcomeBanner(props) {
   return (
     <div>
-        <h1>Benvenuti qui nella nostra e vostra piazza dove utilizziamo meno Euro e più VAL</h1>
-        <h2>Qui con piacere facciamo circolare cose, servizi e competenze spendendo meno Euro o anche senza</h2>
+        <h1 className="row center">Benvenuti qui nella nostra e vostra piazza dove utilizziamo meno Euro e più VAL</h1>
+        <h2 className="row center">Qui con piacere facciamo circolare cose, servizi e competenze spendendo meno Euro o anche senza</h2>
     </div>
   );
 }
