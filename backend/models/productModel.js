d86904a7a7650412c6cab5ab122b7870c931b763
Germán Lugo <g.lugo.dev@gmail.com>
2021-02-23 18:53:43 +0100

    Add field allowComments to model.

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -21,7 +21,7 @@ const productSchema = new mongoose.Schema(
     countInStock: { type: Number, required: true },
     rating: { type: Number, required: true },
     numReviews: { type: Number, required: true },
-    reviews: [reviewSchema],
+    allowComments: { type: Boolean, required: false, default: true}
   },
   {
     timestamps: true,
