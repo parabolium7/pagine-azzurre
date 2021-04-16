06738f1b0a4d0dbb4ea087f20eedcc2280a02d9f
Germán Lugo <g.lugo.dev@gmail.com>
2021-04-16 16:56:38 +0200

    Change text.

--- frontend/src/components/WelcomeBanner.js
+++ frontend/src/components/WelcomeBanner.js
@@ -4,7 +4,7 @@ export default function WelcomeBanner(props) {
   return (
     <div>
         <h1 className="row center">Benvenuti qui nella nostra e vostra piazza dove utilizziamo meno Euro e più VAL</h1>
-        <h2 className="row center">Qui con piacere facciamo circolare cose, servizi e competenze spendendo meno Euro o anche senza</h2>
+        <h2 className="row center">L'unica vera piazza mercato di scambio per tutti i sistemi monetari. Monete speculative: Euro, $, Cripto, ecc E monete non speculative: VAL, Crediti, Din, ecc</h2>
     </div>
   );
 }
