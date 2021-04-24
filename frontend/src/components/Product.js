a37661205fc246c252c2364526c59e7b2352156d
Germán Lugo <g.lugo.dev@gmail.com>
2021-04-24 08:14:48 +0200

    Add link to offerer.

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -13,6 +13,11 @@ export default function Product(props) {
         <Link to={`/product/${product._id}`}>
           <h2>{product.name}</h2>
         </Link>
+        <div>
+            <Link to={`/product/${product._id}`}>
+              { product.seller.seller.name }
+            </Link>
+        </div>
         <Rating
           rating={product.rating}
           numReviews={product.numReviews}
@@ -20,11 +25,6 @@ export default function Product(props) {
         <div className="row start">
           <div className="price euro">€{' '+product.priceEuro}&nbsp;{' e'}</div>
           <div className="price">&nbsp;&nbsp;☯{' '+product.priceVal}</div>
-          <div>
-            <Link to={`/product/${product._id}`}>
-              {/* {seller.seller.name} */}
-            </Link>
-          </div>
         </div>
       </div>
     </div>
