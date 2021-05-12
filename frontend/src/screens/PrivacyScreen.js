ec9a2e04be6573ae855ce100fd18c0bd4c2393db
Germán Lugo <g.lugo.dev@gmail.com>
2021-05-12 17:24:20 +0200

    Add cookies policy.

---
+++ frontend/src/screens/PrivacyScreen.js
@@ -0,0 +1,305 @@
+import React from 'react'
+
+export default function PrivacyScreen() {
+  return (
+    <div classname="privacy">
+      <h1 className="row center">Privacy e cookie</h1>
+      <h2>PERCHÉ QUESTE INFORMAZIONI</h2>
+      <p>Ai sensi dell’ Art. 13 del Regolamento (UE) 2016/679 (“Regolamento Generale sulla Protezione dei
+      Dati” di seguito per brevità “Regolamento UE”) questa pagina descrive le modalità di trattamento
+      dei dati personali degli utenti che consultano i siti web:
+      www.valazco.it
+      www.pagineazzurre.net
+      www.cittadini-volontari.it</p>
+      <h2>TITOLARE DEL TRATTAMENTO</h2>
+      <p>A seguito della consultazione di questo sito possono essere trattati dati relativi a persone identificate
+      o identificabili (congiuntamente in seguito “Dati”) e formeranno oggetto, nel rispetto della
+      normativa sopra richiamata e conformemente agli obblighi di riservatezza cui è ispirata l’attività
+      della nostra Società, del trattamento di cui all’art. 4 del Regolamento UE 679/2016. In particolare si
+      informa che il “Titolare” del loro trattamento è il Banco dei Cittadini Volontari in persona del
+      suo legale rappresentante pro-tempore, Via XXV Aprile 37 –San Mauro To/se 10099 (To) Email:
+      ilbancodeivolontari@libero.it</p>
+      <h2>RESPONSABILE PER LA PROTEZIONE DEI DATI</h2>
+      <p>Il Titolare  ha nominato un Responsabile della Protezione dei Dati contattabile all&#39;indirizzo e-mail:
+      carlanto2006@libero.it oppure all&#39;indirizzo: Progetto Val.Az.Co. - Via XXV Aprile 37 - 10099
+      San Mauro To/se (TO) - Tel 011-8224517</p>
+      <h2>CATEGORIE DI DATI TRATTATI E FINALITA&#39; DEL TRATTAMENTO</h2>
+      <h3>Dati di navigazione</h3>
+      <p>I sistemi informatici e le procedure software preposte al funzionamento di questo sito web
+      acquisiscono, nel corso del loro normale esercizio, alcuni dati personali la cui trasmissione è
+      implicita nell&#39;uso dei protocolli di comunicazione di Internet. Si tratta di informazioni che non sono
+      raccolte per essere associate a interessati identificati, ma che per loro stessa natura potrebbero
+      permettere di identificare gli utenti. In questa categoria di dati rientrano gli indirizzi IP o i nomi a
+      dominio dei computer utilizzati dagli utenti che si connettono al sito, gli indirizzi in notazione URI
+      (Uniform Resource Identifier) delle risorse richieste, l’orario della richiesta, il metodo utilizzato nel
+      sottoporre la richiesta al server, la dimensione del file ottenuto in risposta, il codice numerico
+      indicante lo stato della risposta data dal server (buon fine, errore, ecc.) ed altri parametri relativi al
+      sistema operativo e all&#39;ambiente informatico dell’utente. Questi dati vengono utilizzati al solo fine
+      di ricavare informazioni statistiche anonime sull&#39;uso del sito e per controllarne il corretto
+      funzionamento . I dati potrebbero, inoltre, essere utilizzati  in caso di abusi di utilizzo, per evadere
+      richieste delle competenti Autorità, nell&#39;ambito di indagini o accertamenti di responsabilità in sede
+      giudiziaria.</p>
+      <h3>Dati forniti volontariamente dall&#39;utente.</h3>
+      <p>L’invio facoltativo, esplicito e volontario di dati personali da parte dell’utente ai contatti e nei form
+      di registrazione presenti su questo sito comporta la successiva acquisizione dei dati forniti dal
+      mittente. Tali dati verranno trattati da il Banco dei Cittadini Volontari, prevalentemente con mezzi
+      informatici, al fine di dar seguito a quanto richiesto e proposto dall’utente. Specifiche informative
+      di sintesi sono riportate o visualizzate nelle pagine del sito predisposte per particolari servizi a
+      richiesta.</p>
+      <h2>COOKIE</h2>
+      <p>I cookie sono piccoli file di testo che vengono depositati sul computer dell’utente dai siti che visita.
+      Essi sono principalmente utilizzati al fine di far funzionare i siti web e farli operare in modo più
+      efficiente, nonché di fornire informazioni ai proprietari del sito stesso.
+      Il Banco dei Cittadini Volontari utilizza cookie tecnici, nella misura strettamente necessaria alla
+      fruizione e navigazione del proprio sito web. L’uso di cookie tecnici di sessione in particolare è
+      limitato alla trasmissione di identificativi di sessione (costituiti da numeri casuali generati dal
+      server) necessari per consentire l’esplorazione sicura ed efficiente del sito. Tali cookie evitano il
+      ricorso ad altre tecniche informatiche potenzialmente pregiudizievoli per la riservatezza della
+      navigazione degli utenti e non consentono l’acquisizione di dati personali identificativi dell’utente.
+      Nessun dato personale degli utenti viene in proposito acquisito dal sito.
+      Vengono inoltre utilizzati anche dei cookie persistenti per compilare statistiche anonime che
+      permettano di comprendere, come gli utenti usufruiscono del Sito e di migliorare la struttura e di
+      servizi offerti dallo stesso.
+      Qualora l’utente non desideri ricevere alcun tipo di cookie sul proprio elaboratore, né da questo sito
+      né da altri, può elevare il livello di protezione privacy del proprio browser mediante l’apposita
+      funzione.</p>
+
+      Di seguito nel dettaglio i cookie utilizzati: 
+      Nome del cookie Scadenza
+      Predefinita
+
+      Descrizione del cookie
+
+      Trattamento
+
+      X-IT-LB-ID automaticamente
+      cancellato a
+      chiusura del sito
+
+      Identifica il corretto
+      funzionamento della
+      sessione.
+
+      cookiebar 1 mese Tiene traccia del
+      consenso sui Cookie
+      stessi
+
+      eZSESSID automaticamente
+      cancellato a
+      chiusura del sito
+
+      Identifica il corretto
+      funzionamento della
+      sessione.
+
+       
+
+      is_logged_in automaticamente
+      cancellato a
+
+      Identifica il corretto
+      funzionamento della
+       
+
+      chiusura del sito sessione.
+
+      Lang automaticamente
+      cancellato a
+      chiusura del sito
+       
+      Identifica il paese in
+      cui si trova l&#39;utente
+
+      Per avere maggiori informazioni
+      consultare la Privacy Policy di
+      Twitter
+
+      personalization_id 2 anni Cookie è impostato  per
+      l&#39;integrazione di Twitter
+      e delle funzionalità di
+      condivisione per i
+      social media
+
+      Per avere maggiori informazioni
+      consultare la Privacy Policy di
+      Twitter
+
+      guest_id 2 anni Cookie è impostato  per
+      l&#39;integrazione di Twitter
+      e delle funzionalità di
+      condivisione per i
+      social media
+
+      Per avere maggiori informazioni
+      consultare la Privacy Policy di
+      Twitter
+
+      1P_JAR 1 mese Memorizza le
+      preferenze e le
+      informazioni dell’utente
+      ogni volta che visita
+      pagine web contenenti
+      mappe geografiche di
+      Google Maps
+
+      Per avere maggiori informazioni
+      consultare la Privacy Policy
+      di Google 
+
+      has_js automaticamente
+      cancellato a
+      chiusura del sito
+
+      Tecnico, verifica lo
+      stato di attivazione di
+      javascript sul browser
+       
+
+      _gid 24 hours Tecnico, usato per
+      distinguere gli utenti.
+       
+
+      <p>Si comunica inoltre che il Banco dei Cittadini Volontari si avvale delle funzionalità offerte dal
+      servizio Google Analytics, che utilizza i cookie per raccogliere e analizzare in forma anonima le
+      informazioni sui comportamenti di utilizzo dei siti web (ad esempio l’ora e il giorno in cui si è
+      verificata la visita, eventuali precedenti visite al sito da parte dello stesso visitatore, il sito di
+      provenienza, etc.). Tali informazioni (compreso l’indirizzo IP dell’utente) vengono raccolte da
+      Google Analytics, che le elabora allo scopo di redigere report per il Banco dei Cittadini Volontari
+      riguardanti le attività sul proprio sito web e utilizzate da questa al fine di migliorare il sito stesso.
+      Google non associa l’indirizzo IP a nessun altro dato in proprio possesso né cerca di collegare un
+      indirizzo IP con l’identità di un utente. Google può anche comunicare queste informazioni a terzi
+      ove ciò sia imposto dalla legge o laddove tali terzi trattino le suddette informazioni per conto di
+      Google.</p>
+      Di seguito nel dettaglio.
+      Cookie Nome Scadenza
+      predefinita
+
+      Descrizione del cookie Trattamento
+
+      Google
+      Analytics
+
+      _gat A fine sessione analisi anonima delle
+      statistiche di navigazione
+       
+      _ga 2 anni  
+      _utma Per avere
+      maggiori
+      informazioni
+      consultare la
+      Privacy Policy di
+      Google
+
+      Questi cookie vengono
+      utilizzati per raccogliere
+      informazioni su come i
+      visitatori utilizzano il sito
+      (ad esempio l’ora e il
+      giorno in cui si è verificata
+      Per avere
+      maggiori
+      informazioni
+      consultare la
+      Privacy Policy di
+      Google
+
+      _utmb
+      _utmc
+      _utmz
+
+      la visita, eventuali
+      precedenti visite al sito da
+      parte dello stesso
+      visitatore, il sito di
+      provenienza, etc.). Tali
+      informazioni vengono
+      utilizzate da
+      pagineazzurre.net al fine
+      di migliorare il sito stesso
+
+      Per maggiori informazioni in merito alle modalità di raccolta e utilizzo dei dati da parte di Google si
+      raccomanda la consultazione del sito: www.google.it/policies/privacy/partners/ (o ad altra URL che
+      Google potrà fornire di volta in volta).
+      Qualora l’utente non desideri ricevere alcun tipo di cookie sul proprio elaboratore, né da questo sito
+      né da altri, può elevare il livello di protezione privacy del proprio browser mediante l’apposita
+      funzione.
+      <h2>GESTIONE DEI COOKIE</h2>
+      Si ricorda che disabilitare totalmente o parziale i cookie c.d. tecnici può compromettere l’utilizzo
+      delle funzionalità del sito. Ad ogni modo l’utente può decidere se accettare o meno i cookie
+      modificando le impostazioni di sicurezza del suo browser.
+      Mozilla Firefox: https://support.mozilla.org/it/kb/Gestione%20dei%20cookie
+      Google Chrome: https://support.google.com/chrome/answer/95647?hl=it
+      Internet Explorer: http://windows.microsoft.com/it-it/windows-vista/block-or-allow-cookies
+      Safari 6/7 Mavericks: https://support.apple.com/kb/PH17191?viewlocale=it_IT&amp;locale=it_IT
+      Safari 8 Yosemite: https://support.apple.com/kb/PH19214?viewlocale=it_IT&amp;locale=it_IT
+      Safari su iPhone, I Pad o iPod touch: https://support.apple.com/it-it/HT201265 
+      Si ricorda che è possibile disabilitare in modo selettivo l’azione di Google Analytics installando sul
+      proprio browser la componente di opt-out fornita da Google. Per disabilitare l’azione di Google
+      Analytics, si rinvia al link di seguito indicato: https://tools.google.com/dlpage/gaoptout 
+      <h2>LINK AD ALTRI SITI WEB</h2>
+      Questo sito potrebbe contenere link o riferimenti per l’accesso ad altri siti, quali ad esempio i social
+      network Facebook, Instagram, Google+, Twitter e Pinterest. Cliccando sugli appositi link si potrà
+      ad esempio condividere i nostri contenuti. Informiamo che il Titolare del trattamento non controlla i
+      cookie o le altre tecnologie di monitoraggio di tali siti web ai quali la presente Policy non si applica.
+      <h2>FACOLTATIVITA’ DEL CONFERIMENTO DEI DATI</h2>
+      A parte quanto specificato per i dati di navigazione, l’utente è libero di fornire i dati propri
+      personali. Tuttavia il loro mancato conferimento può comportare l’impossibilità di ottenere quanto
+      richiesto.
+      <h2>MODALITA’ DEL TRATTAMENTO E TEMPI DI CONSERVAZIONE</h2>
+      I trattamenti connessi ai servizi web di questo sito avvengono in cloud e/o su server ubicati
+      all&#39;interno dell’Unione Europea, di proprietà e/o nella disponibilità del Titolare e/o di società terze 
+      debitamente nominate quali Responsabili del trattamento. I dati  sono curati solo da personale
+      tecnico interno autorizzato al trattamento o da fornitori terzi nominati quali Responsabili del
+      trattamento per occasionali operazioni di manutenzione. I dati personali sono conservati  per il
+      tempo strettamente necessario a conseguire gli scopi per cui sono stati raccolti e comunque non
+      oltre i tempi di conservazione previsti dalla normativa vigente. Specifiche misure di sicurezza sono
+      osservate per prevenire la perdita dei dati, usi illeciti o non corretti ed accessi non autorizzati.
+      <strong>Il Banco dei Cittadini Volontari</strong> ha adottato misure di sicurezza ispirandosi ai principali standard
+
+      internazionali. Ha inoltre adottato misure tecniche ed organizzative adeguate per ridurre al minimo i
+      rischi afferenti la riservatezza, la disponibilità e l’integrità dei dati personali raccolti e trattati.
+      <h2>COMUNICAZIONE E DIFFUSIONE DEI DATI</h2>
+      I dati raccolti potranno essere comunicati ad altre società per attività strettamente connesse e
+      strumentali all&#39;operatività del servizio, come la gestione del sistema informatico o la gestione delle
+      richieste degli utenti. Al di fuori di questi casi, i dati personali non saranno comunicati, né concessi
+      ad alcuno, salvo previsione contrattuale o autorizzazione dei soggetti interessati. In questo senso, i
+      dati personali potrebbero essere trasmessi a terze parti, ma solo ed esclusivamente nel caso in cui: a)
+      vi sia consenso esplicito a condividere i dati con terze parti; b) vi sia la necessità di condividere con
+      terzi le informazioni al fine di prestare il servizio richiesto; c) ciò sia necessario per adempiere a
+      richieste dell’Autorità Giudiziaria o di Pubblica Sicurezza. Nessun dato derivante dal servizio web
+      viene diffuso.
+      <h2>DIRITTI DEGLI INTERESSATI</h2>
+      La normativa a protezione dei dati personali prevede espressamente alcuni diritti in capo ai soggetti
+      cui i dati si riferiscono (c.d. interessato). In particolare, in relazione al trattamento dei predetti dati,
+      in base all&#39;art. 15 e ss. del Regolamento, Vi ricordiamo il diritto di ottenere l’accesso ai dati
+      personali che vi riguardano; la rettifica dei dati (correzione di dati inesatti od integrazione), la
+      cancellazione dei dati trattati in violazione di legge o qualora sussista uno dei motivi specificati
+      dall&#39;articolo 17 del GDPR, nonché il diritto alla portabilità dei dati stessi. Voi avete altresì, il diritto
+      di proporre reclamo all&#39;autorità di controllo qualora ritenesse che i diritti qui indicati non siano stati
+      effettivamente riconosciuti.
+      Per esercitare i diritti potrete rivolgervi al Titolare del trattamento scrivendo a Il Banco dei Cittadini
+      Volontari, Via XXV Aprile 37 – 10099 San Mauro To/se (TO), Email:
+      ilbancodeivolontari@libero.it . In alternativa putete rivolgersi al Responsabile per la Protezione dei
+      Dati o Data Protection Officer inviando un messaggio di posta elettronica all&#39;indirizzo
+      carlanto2006@libero.it
+      <h2>MODIFICHE ALLE PRESENTI POLITICHE SULLA PRIVACY</h2>
+      Il Banco dei Cittadini Volontari verifica periodicamente la propria politica relativa alla privacy e
+      alla sicurezza e, se del caso, la rivede in relazione alle modifiche normative, organizzative o dettate
+      dall&#39;evoluzione tecnologica. In caso di modifica delle politiche, la nuova versione sarà pubblicata in
+      questa pagina del sito
+      <h2>DOMANDE, RECLAMI E SUGGERIMENTI</h2>
+      Chiunque fosse interessato a maggiori informazioni, a contribuire con propri suggerimenti o
+      avanzare reclami o contestazioni in merito alle politiche privacy o sul modo in cui la nostra Società
+      tratta i dati personali, può effettuarlo scrivendo a il Banco dei Cittadini Volontari, Via XXV
+      Aprile 37 – 10099 San Mauro To/se (TO), Email: ilbancodeivolontari@libero.it o un messaggio
+      di posta elettronica all&#39;indirizzo carlanto2006@libero.it
+
+      <h2>LEGGE E GIURISDIZIONE</h2>
+      L’interpretazione e l’esecuzione di queste condizioni sono regolate dalla legge italiana. Il Foro di
+      Torino (Italia), sarà competente in via esclusiva per qualsiasi controversia comunque connessa a
+      queste condizioni, salvo ove non diversamente previsto per legge. Il Titolare si riserva di ottenere
+      rimedi urgenti presso qualunque Corte anche all&#39;estero, per proteggere i propri interessi e far
+      rispettare i propri diritti.
+      Ultimo aggiornamento: 12/05/2021
+</div>)
+}
