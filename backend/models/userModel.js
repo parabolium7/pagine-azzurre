b19b8fe547bb0f5abc58a6ce0eacbf369551a33d
Germán Lugo <g.lugo.dev@gmail.com>
2021-05-22 09:26:18 +0200

    Fix style.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema(
     birthplace: { type: String, required: false, uppercase: true },
     gender: { type: String, enum:["M", "F"], required: false },
     cf: { type: String, required: false, unique: true, uppercase: true },
+    partitaIva: { type: String, required: false, unique: true },
     email: { type: String, required: true, unique: true, trim: true},
     city: { type: String, required: false, uppercase: true },
     zipCode: { type: Number, required: false },
