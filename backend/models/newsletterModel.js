0713e68e10e223fbe2f711fab735a7037c827fe6
Germán Lugo <g.lugo.dev@gmail.com>
2021-06-25 13:30:49 +0200

    Add field.

--- backend/models/newsletterModel.js
+++ backend/models/newsletterModel.js
@@ -5,7 +5,8 @@ const newsletterSchema = new mongoose.Schema(
     name: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     citybase: { type: String, required: false },
-    verified: { type: Boolean, required: true, default: false}
+    verified: { type: Boolean, required: true, default: false },
+    newsnumber: { type: Number, required: true, default: 0 },  // TODO: Future implementation.
   },
   {
     timestamps: true,
