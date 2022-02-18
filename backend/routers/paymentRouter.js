3ec7db63bf657bc744818c4c7fddb43a5e47b45b
Germán Lugo <g.lugo.dev@gmail.com>
2022-02-18 13:17:23 +0100

    Fix smart contract address.

--- backend/routers/paymentRouter.js
+++ backend/routers/paymentRouter.js
@@ -22,19 +22,18 @@ paymentRouter.post(
     const buyerPvKey = buyer[0].accountKey
     const valsToSend = (req.body.item.reduce((a, c) => a + c.priceVal * c.qty, 0)) * 100
     const InfuraUrl = process.env.INFURA_URL
-    const provider = new HDWalletProvider(buyerPvKey, InfuraUrl)
+    const provider = new HDWalletProvider([buyerPvKey], InfuraUrl)
     const web3 = new Web3(provider)
     const id = 5
     const deployedNetwork = contract.networks[id]
     const sContractInstance = new web3.eth.Contract(contract.abi, deployedNetwork.address)
     console.log("Vals to send", valsToSend)
     sContractInstance.methods.transfer(seller[0].account, valsToSend).send({from: buyerPbKey })
-      .then( resp=> {
+      .then( resp => {
         console.log("Tx: ", resp.transactionHash)
         if(typeof resp.transactionHash === 'string') response = res
       })
       .catch( err => console.log("ERROR PAYMENT:", err))
-      setInterval( () => console.log("Done!", ), 15000)
     res.send(response.event)
   })
 )
