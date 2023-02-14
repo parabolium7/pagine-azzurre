30bc1f893e89ded27de552308f4033a96895e789
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2023-02-14 16:44:40 +0200

    fixed bug in Product where price did not show up

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,36 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Rating from './Rating';
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
-export default function Product(props) {
-  const { product } = props;
+export default function Product({ product }) {
   return (
     <div key={product._id} className="card">
       <Link to={`/product/${product._id}`}>
         <img className="medium" src={product.image[0]} alt={product.name} />
       </Link>
-      <div className="row center">{ product.section !== 'avviso' && product.section !== 'propongo' ? (product.isService?'Servizio':'Prodotto'):'Avviso/Proposta' }</div>
+      <div className="row center">
+        {product.section !== "avviso" && product.section !== "propongo"
+          ? product.isService
+            ? "Servizio"
+            : "Prodotto"
+          : "Avviso/Proposta"}
+      </div>
       <div className="card-body">
         <Link to={`/product/${product._id}`}>
           <h2>{product.name}</h2>
         </Link>
         <div>
-            <Link to={`/product/${product._id}`}>
-              { product.seller.seller.name }
-            </Link>
+          <Link to={`/product/${product._id}`}>
+            {product.seller.seller.name}
+          </Link>
         </div>
         <Rating
           rating={product.rating}
           numReviews={product.numReviews}
         ></Rating>
-          { product.section !== 'propongo' || product.section !== 'avviso' &&
-            (
-              <div className="row start">
-                <div className="price euro">€{' '+product.priceEuro}&nbsp;{' e'}</div>
-                <div className="price">&nbsp;&nbsp;☯{' '+product.priceVal}</div>
-              </div>
-            )
-          }
+        {console.log(product)}
+        {(product.section !== "propongo" || product.section !== "avviso") && (
+          <div className="row start">
+            <div className="price euro">
+              €{" " + product.priceEuro}&nbsp;{" e"}
+            </div>
+            <div className="price">&nbsp;&nbsp;☯{" " + product.priceVal}</div>
+          </div>
+        )}
       </div>
     </div>
   );
