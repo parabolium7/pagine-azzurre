18acde42a83a8232c9559795370e664bb63f99a8
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2023-02-14 13:34:50 +0200

    responsive card images

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import WelcomeBanner from '../components/WelcomeBanner'
+import React, { useEffect, useState } from "react";
+import WelcomeBanner from "../components/WelcomeBanner";
 // import { Carousel } from 'react-responsive-carousel/lib/styles/carousel.min.css'
-import Product from '../components/Product'
-import LoadingBox from '../components/LoadingBox'
-import MessageBox from '../components/MessageBox'
-import { useDispatch, useSelector } from 'react-redux';
-import { listProducts } from '../actions/productActions';
+import Product from "../components/Product";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 // import { listTopSellers } from '../actions/userActions';
-import CookieConsent from "react-cookie-consent"
-import { Link } from 'react-router-dom';
+import CookieConsent from "react-cookie-consent";
+import { Link } from "react-router-dom";
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
@@ -22,7 +22,7 @@ export default function HomeScreen() {
   //   users: sellers,
   // } = userTopSellersList;
 
-  const [section, setSection] = useState('offro')
+  const [section, setSection] = useState("offro");
 
   useEffect(() => {
     dispatch(listProducts({}));
@@ -30,22 +30,30 @@ export default function HomeScreen() {
   }, [dispatch]);
   return (
     <div>
-      <WelcomeBanner/> 
-        <CookieConsent
-          location="bottom"
-          buttonText="Accetto"
-          cookieName="pagineazzurre"
-          style={{ background: "#626360" }}
-          buttonStyle={{ color: "#626360", fontSize: "15px", marginRight: "150px" }}
-          expires={150}
-        >
-          I cookie ci aiutano ad erogare servizi di qualità.<br></br>
-          Utilizzando i nostri servizi, l'utente accetta le nostre modalità d'uso dei cookie.
-          Qualora l’utente non desideri ricevere alcun tipo di cookie sul proprio elaboratore, né da questo sito né da altri, può elevare il livello di protezione privacy del proprio browser mediante l’apposita funzione,  come specificato di seguito.
-          Le pagineazzurre.net fa uso dei cookie. L'informazione più dettagliata nella nostra pagina sulla <Link to="/privacy">Privacy</Link>
-        </CookieConsent>
+      <WelcomeBanner />
+      <CookieConsent
+        location="bottom"
+        buttonText="Accetto"
+        cookieName="pagineazzurre"
+        style={{ background: "#626360" }}
+        buttonStyle={{
+          color: "#626360",
+          fontSize: "15px",
+          marginRight: "150px",
+        }}
+        expires={150}
+      >
+        I cookie ci aiutano ad erogare servizi di qualità.<br></br>
+        Utilizzando i nostri servizi, l'utente accetta le nostre modalità d'uso
+        dei cookie. Qualora l’utente non desideri ricevere alcun tipo di cookie
+        sul proprio elaboratore, né da questo sito né da altri, può elevare il
+        livello di protezione privacy del proprio browser mediante l’apposita
+        funzione, come specificato di seguito. Le pagineazzurre.net fa uso dei
+        cookie. L'informazione più dettagliata nella nostra pagina sulla{" "}
+        <Link to="/privacy">Privacy</Link>
+      </CookieConsent>
       <div>
-      {/* <h2>Top Sellers</h2>
+        {/* <h2>Top Sellers</h2>
       {loadingSellers ? (
         <LoadingBox></LoadingBox>
       ) : errorSellers ? (
@@ -64,32 +72,58 @@ export default function HomeScreen() {
             ))}
           </Carousel> */}
         </>
-      {/* )} */}
-      <h1 className="row center">ULTIME ATTIVITÀ</h1>
-      <div className="row center">
-        <button className={ section === 'offro' ? "filter active":"filter"} onClick={()=> setSection('offro')}>Offerte</button>
-        <button className={ section === 'cerco' ? "filter active" : "filter"} onClick={()=> setSection('cerco')}>Richieste</button>
-        <button className={ section === 'propongo' ? "filter active" : "filter"} onClick={()=> setSection('propongo')}>Proposte</button>
-        <button className={ section === 'avviso' ? "filter active" : "filter"} onClick={()=> setSection('avviso')}>Avvisi</button>
-      </div>
-      {loading ? (
-        <LoadingBox></LoadingBox>
-      ) : error ? (
-        <MessageBox variant="danger">{error}</MessageBox>
-      ) : (
-        <>
-          {products.length === 0 && <MessageBox>No Product Found</MessageBox>}
-          <div className="row center">
-            { products.map((product) => {
-              // let expired = new Date(product.expiry)
-              // let now = new Date()
-              // const isExpired = expired.getTime() <= now
-              return (!product.name.match(/Annunciø/) && product.section === section && !product.pause && <Product key={product._id} product={product}></Product>)
+        {/* )} */}
+        <h1 className="row center">ULTIME ATTIVITÀ</h1>
+        <div className="row center">
+          <button
+            className={section === "offro" ? "filter active" : "filter"}
+            onClick={() => setSection("offro")}
+          >
+            Offerte
+          </button>
+          <button
+            className={section === "cerco" ? "filter active" : "filter"}
+            onClick={() => setSection("cerco")}
+          >
+            Richieste
+          </button>
+          <button
+            className={section === "propongo" ? "filter active" : "filter"}
+            onClick={() => setSection("propongo")}
+          >
+            Proposte
+          </button>
+          <button
+            className={section === "avviso" ? "filter active" : "filter"}
+            onClick={() => setSection("avviso")}
+          >
+            Avvisi
+          </button>
+        </div>
+        {loading ? (
+          <LoadingBox></LoadingBox>
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <>
+            {products.length === 0 && <MessageBox>No Product Found</MessageBox>}
+            <div className="row center">
+              {products.map((product) => {
+                // let expired = new Date(product.expiry)
+                // let now = new Date()
+                // const isExpired = expired.getTime() <= now
+                return (
+                  !product.name.match(/Annunciø/) &&
+                  product.section === section &&
+                  !product.pause && (
+                    <Product key={product._id} product={product}></Product>
+                  )
+                );
               })}
-          </div>
-        </>
-      )}
-    </div>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
